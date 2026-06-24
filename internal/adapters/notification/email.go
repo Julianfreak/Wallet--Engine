45acdf15972d83422c2e4399d0335c5c349e5861
Julian Serna <analistadesarrollo1@flotalamacarena.com>
2026-06-24 14:02:22 -0500

    feat: agregar el puerto NotificationSender al TransferService y ejecutar el envío de forma síncrona

--- internal/adapters/notification/email.go
+++ internal/adapters/notification/email.go
@@ -14,10 +14,10 @@ func NewEmailSender() *EmailSender {
 
 // Send simula una conexión de red lenta de 3 segundos hacia un servidor de correos
 func (s *EmailSender) Send(recipient string, message string) error {
-	// ⏳ SIMULACIÓN DE LATENCIA: Aquí es donde la app normalmente se quedaría congelada
+	// SIMULACIÓN DE LATENCIA: Aquí es donde la app normalmente se quedaría congelada
 	time.Sleep(3 * time.Second)
 
 	// \033[33m activa el color amarillo para diferenciarlo de la auditoría azul
-	fmt.Printf("📧 \033[33m[EMAIL ENVIADO]\033[0m Notificación exitosa para %s: %s\n", recipient, message)
+	fmt.Printf("\033[33m[EMAIL ENVIADO]\033[0m Notificación exitosa para %s: %s\n", recipient, message)
 	return nil
 }
--- internal/application/transfer-service.go
+++ internal/application/transfer-service.go
@@ -14,14 +14,22 @@ type TransferService struct {
 	transactionRepo ports.TransactionRepository
 	txManager       ports.TxManager
 	logger          ports.Logger
+	notifier        ports.NotificationSender
 }
 
-func NewTransferService(ar ports.AccountRepository, tr ports.TransactionRepository, tm ports.TxManager, log ports.Logger) *TransferService {
+func NewTransferService(
+	ar ports.AccountRepository,
+	tr ports.TransactionRepository,
+	tm ports.TxManager,
+	log ports.Logger,
+	notif ports.NotificationSender, // <-- Quinto argumento
+) *TransferService {
 	return &TransferService{
 		accountRepo:     ar,
 		transactionRepo: tr,
 		txManager:       tm,
 		logger:          log,
+		notifier:        notif, // <-- Asignamos el notificador
 	}
 }
 
@@ -66,6 +74,9 @@ func (s *TransferService) Execute(ctx context.Context, fromID, toID string, amou
 
 		msg := fmt.Sprintf("Transferencia exitosa de %s a %s por un monto de $%.2f. (ID Auditoría: %s)", fromID, toID, amount, txID)
 		s.logger.Info(msg)
+
+		emailMsg := fmt.Sprintf("¡Hola! Recibiste una transferencia de $%.2f de la cuenta %s.", amount, fromID)
+		_ = s.notifier.Send("usuario_mercado_libre@email.com", emailMsg)
 
 		return nil // Si llegamos aquí sin errores, el TxManager hace COMMIT automáticamente
 	})
